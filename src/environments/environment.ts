dfa25fdc9aa04931f0eb9b4f31db06cda4c98f40
Luke Reinsch <reinschla@gmail.com>
2020-03-28 16:00:00 -0500

    Fix environment api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // api: 'http://localhost:3000'
-  api: 'https://api.coroname.net'
+  api: 'http://localhost:3000'
+  // api: 'https://api.coroname.net'
 };
 
 /*
