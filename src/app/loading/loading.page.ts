c0e7127b50486bc96d55adc0c367814de5e29884
Daniel Noon <supercookies@outlook.com>
2020-03-28 14:55:47 -0500

    change readme to readme.md

--- src/app/loading/loading.page.ts
+++ src/app/loading/loading.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-loading',
@@ -8,12 +10,19 @@ import { Router } from '@angular/router';
 })
 export class LoadingPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private api: ApiService) { }
 
   ngOnInit() {
+    this.init();
+  }
+
+  async init() {
     if (localStorage.getItem('token')) {
-      
-      this.router.navigateByUrl('/home');
+      if (await User.init(this.api)) {
+        this.router.navigateByUrl('/home');
+      } else {
+        this.router.navigateByUrl('/login');
+      }
     } else {
       this.router.navigateByUrl('/login');
     }
