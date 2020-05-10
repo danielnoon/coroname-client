0b52eade156c9dd92217cc0cf61d0313a5615128
Daniel Noon <danielanoon@outlook.com>
2020-05-10 17:31:52 -0500

    update theme, add icons, app manifest, enable pwa (#5)

--- src/app/user-popover/user-popover.component.ts
+++ src/app/user-popover/user-popover.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../user';
-import { PopoverController } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { User } from "../user";
+import { PopoverController } from "@ionic/angular";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-user-popover',
-  templateUrl: './user-popover.component.html',
-  styleUrls: ['./user-popover.component.scss'],
+  selector: "app-user-popover",
+  templateUrl: "./user-popover.component.html",
+  styleUrls: ["./user-popover.component.scss"],
 })
 export class UserPopoverComponent implements OnInit {
   admin = false;
+  theme = "light";
+  defaultTheme = "light";
+  nonDefaultTheme = false;
 
-  constructor(private pop: PopoverController, private router: Router) { }
+  constructor(private pop: PopoverController, private router: Router) {}
 
   ngOnInit() {
     if (User.me) {
@@ -21,6 +24,19 @@ export class UserPopoverComponent implements OnInit {
     User.listen(() => {
       this.admin = User.me.admin;
     });
+
+    const darkThemeDefault = matchMedia
+      ? matchMedia("prefers-color-scheme: dark")
+      : false;
+    if (darkThemeDefault) {
+      this.defaultTheme = "dark";
+    } else {
+      this.defaultTheme = "light";
+    }
+    this.theme = localStorage.getItem("theme") || this.defaultTheme;
+    if (this.theme !== this.defaultTheme) {
+      this.nonDefaultTheme = true;
+    }
   }
 
   dismiss() {
@@ -29,7 +45,25 @@ export class UserPopoverComponent implements OnInit {
 
   logOut() {
     localStorage.clear();
-    this.router.navigateByUrl('/');
+    this.router.navigateByUrl("/");
     this.dismiss();
+  }
+
+  switchTheme() {
+    if (this.theme === "light") {
+      this.theme = "dark";
+      localStorage.setItem("theme", "dark");
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+    } else {
+      this.theme = "light";
+      localStorage.setItem("theme", "light");
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    }
+
+    if (this.theme !== this.defaultTheme) {
+      this.nonDefaultTheme = true;
+    }
   }
 }
