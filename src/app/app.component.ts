e831dfc12d056f9d7fa9026d4311657ed8d6f91e
Daniel Noon <supercookies@outlook.com>
2020-07-04 14:15:04 -0500

    Add license

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { User } from './user';
-import { ApiService } from './api.service';
-import { Router } from '@angular/router';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
   constructor(
